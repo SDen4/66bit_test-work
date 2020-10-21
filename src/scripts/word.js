2f85612d7c1dd25ec344e0ae0bf0b3276d10e0f7
SDen4 <asw325@yandex.ru>
2020-10-21 22:19:02 +0500

    js

--- src/scripts/word.js
+++ src/scripts/word.js
@@ -1,3 +1,17 @@
-let add = (a,b) => a+b;
+const wrapper = document.querySelector('.word__wrapper');
 
-console.log(add(2,3));
+window.addEventListener("keydown", function (e) {
+
+    //disable pushing any keys excluding letter
+    if(!e.key.match(/[A-Za-zА-Яа-яёЁ]/) || 
+        e.key.match(/F[1-9]/g) || 
+        e.key.match(/[:cntrl:]/) || 
+        e.key.match(/[:blank:]/)) 
+    { return };
+
+    //creation new element for each letter
+    let elem = document.createElement('div');
+    elem.className = "word__letter";
+    elem.textContent = e.key;
+    wrapper.append(elem);
+});
